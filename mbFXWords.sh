1ede792d301da960973f4eb7f3e7cdcdce77701d
open-hopin <message@sensalgo.com>
2018-11-24 21:01:25 +0100

    Build v1.04 manual

--- mbFXWords.sh
+++ mbFXWords.sh
@@ -4,5 +4,6 @@
 #echo "Script location: ${BASEDIR}"
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 #echo $DIR
-java -splash:$DIR/resources/FlatXoreo.png -jar $DIR/mbFXWords.jar "$1" $2
+#java -splash:$DIR/resources/FlatXoreo.png -jar $DIR/mbFXWords.jar "$1" $2
+java -splash:$DIR/resources/FlatXoreo.png -jar $DIR/mbFXWords.jar $1 $2
 read -p 'Press Enter to close shell...' var
